c8e5855fc19a32308c5da3156a4df6c3b93405b4
zzhang391 <lindaz@gatech.edu>
2017-04-20 22:45:43 -0400

    Implemented 'help out' button logic

--- src/app/progress-bars/progress-bars.component.ts
+++ src/app/progress-bars/progress-bars.component.ts
@@ -22,7 +22,20 @@ export class ProgressBarsComponent implements OnInit {
   progress_donation: number; // Percentage of progress (0 - 1.0)
   needed_donation: number; // Number of donations needed
 
+  types : string[] = ['volunteer', 'funding', 'donation'];
+
   ngOnInit(): void {
+    this.calculateProgress();
+  }
+
+  helpOut(resource_type: string): void {
+    this.resources.find(resource => {
+      return resource.type == resource_type;
+    }).current_number += 1;
+    this.calculateProgress();
+  }
+
+  calculateProgress(): void {
     var ideal_sum = {
       'volunteer':0,
       'funding' : 0,
@@ -38,8 +51,7 @@ export class ProgressBarsComponent implements OnInit {
       ideal_sum[resource.type] += resource.ideal_number;
       current_sum[resource.type] += resource.current_number;
     });
-    var types = ['volunteer', 'funding', 'donation'];
-    types.forEach(type => {
+    this.types.forEach(type => {
       var key = ['progress_', type].join('')
       this[key] = Math.round(current_sum[type]/ideal_sum[type]*100);
 
@@ -49,11 +61,11 @@ export class ProgressBarsComponent implements OnInit {
       this[key] = ideal_sum[type]-current_sum[type];
     })
 
-  // console.log("progress_volunteer = " + this.progress_volunteer);
-  // console.log("needed_volunteer = " + this.needed_volunteer);
-  // console.log("progress_funding = " + this.progress_funding);
-  // console.log("needed_funding = " + this.needed_funding);
-  // console.log("progress_donation = " + this.progress_donation);
-  // console.log("needed_donation = " + this.needed_donation);
+  console.log("progress_volunteer = " + this.progress_volunteer);
+  console.log("needed_volunteer = " + this.needed_volunteer);
+  console.log("progress_funding = " + this.progress_funding);
+  console.log("needed_funding = " + this.needed_funding);
+  console.log("progress_donation = " + this.progress_donation);
+  console.log("needed_donation = " + this.needed_donation);
   }
 }
