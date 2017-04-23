e9f5dd1522f6e2b596480b4b17f6df84b772a868
michelle-chiu <michellechiu96@gmail.com>
2017-04-23 17:04:15 -0400

    homepage project changes

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -13,20 +13,12 @@ const piedmont_park_required_resources: Resource[] = [
 const inman_park_required_resources: Resource[] = [
     {
         id: 0,
-        type: "volunteer",
+        type: "funding",
         name: "Attandant",
         ideal_number: 30,
         current_number: 14,
         contributions: []
-    },
-    {
-        id: 0,
-        type: "funding",
-        name: "Attandant",
-        ideal_number: 30,
-        current_number: 0,
-        contributions: []
-    },
+    }
 ]
 const botanic_garden_required_resources: Resource[] = [
     {
@@ -56,6 +48,27 @@ const womens_march_required_resources: Resource[] = [
         contributions: []
     },
 ]
+const beekeeping_required_resources: Resource[] = [
+    {
+        id: 0,
+        type: "volunteer",
+        name: "Attandant",
+        ideal_number: 100,
+        current_number: 48,
+        contributions: []
+    },
+]
+const hike_required_resources: Resource[] = [
+    {
+        id: 0,
+        type: "funding",
+        name: "Attandant",
+        ideal_number: 20,
+        current_number: 18,
+        contributions: []
+    },
+
+]
 
 export const PROJECTS: Project[] = [
   {
@@ -83,6 +96,25 @@ export const PROJECTS: Project[] = [
   },
   {
     id: 1,
+    title: `Beekeeping Event at Metro Atlanta Beekeepers Association`,
+    userID: 0,
+    userName: 'Emily Smith',
+    description: `Hi all, the Metro Atlanta Beekeepers Association is hosting a free beekeeping event to educate the community about why keeping the bee population plentiful is imperative to the environment. Join us if you can!`,
+    date: `Monday, May 25th, 2017`,
+    timeOfEvent: '1:00 pm - 4:00 pm',
+    timeLeft: 14,
+    category: 2,
+    tag: 'Park, Environment, Beekeeping, Gardens',
+    media: ``,
+    neighborhood: 0,
+    locationName: 'Atlanta Botanical Gardens',
+    address: "400 Peachtree Dr NE, 18th Street",
+    thumbnail: `./../../assets/thumbnails/beekeeping.jpg`,
+    resources_deadline: ``,
+    resources: beekeeping_required_resources
+  },
+  {
+    id: 2,
     title: `Inman Park Bike Meetup`,
     userID: 0,
     userName: 'Jane Doe',
@@ -104,8 +136,8 @@ export const PROJECTS: Project[] = [
     resources: inman_park_required_resources
   },
   {
-    id: 2,
-    title: `Volunteering at Atlanta's Botanical Garden`,
+    id: 3,
+    title: `Beautifying Atlanta's Botanical Garden`,
     userID: 0,
     userName: `Jane Doe`,
     description: `Hi all, I'm looking for volunteers to help set up food and drinks for
@@ -127,25 +159,23 @@ export const PROJECTS: Project[] = [
     resources: botanic_garden_required_resources
   },
   {
-    id: 3,
-    title: `Women's March`,
+    id: 4,
+    title: `Hiking at Stone Mountain`,
     userID: 0,
-    userName: 'Jane Doe',
-    description: `Hi all, I'm looking for people to go to the Women's March with! Please
-    try to be here by 10:50 AM. If you have any questions, please reach out to me at
-    janedoe3710@gmail.com. Please spread the word!`,
-    date: `Monday, May 15th, 2017`,
-    timeOfEvent: '11:00 am - 2:00 pm',
-    timeLeft: 14,
+    userName: 'John Adams',
+    description: `Hi everyone! I just moved to the city and was looking to meet some people in the area. Looking forward to meeting everyone at the hike!`,
+    date: `Tuesday, May 16th, 2017`,
+    timeOfEvent: '10:00 am - 2:00 pm',
+    timeLeft: 6,
     category: 2,
     tag: 'Park, Environment',
     media: ``,
     neighborhood: 0,
-    locationName: 'City Hall',
-    address: "400 Park Dr NE, 14th Street",
-    thumbnail: `./../../assets/thumbnails/womens_march.png`,
+    locationName: 'Stone Mountain',
+    address: "400 Peachtree Road, Front Entrance",
+    thumbnail: `./../../assets/thumbnails/hike.jpg`,
     resources_deadline: ``,
-    resources: womens_march_required_resources
+    resources: hike_required_resources
   }
 ];
 
